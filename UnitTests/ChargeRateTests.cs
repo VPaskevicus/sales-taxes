1cef14ac819a8639b86157e81989c5eea2179a62
VPaskevicus <v.paskevicus@outlook.com>
2017-03-17 22:28:17 +0000

    #6 - Adding unit tests for the product and charge rate classes.

---
+++ UnitTests/ChargeRateTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ST.BusinessLogic.Tests
+{
+    /// <summary>
+    /// The charge rate class tests
+    /// </summary>
+    [TestClass]
+    public class ChargeRateTests
+    {
+        /// <summary>
+        /// Create charge rate using parameterized constructor.
+        /// </summary>
+        [TestMethod]
+        public void ChargeRate_Create_Success()
+        {
+            var chargeRate = new ChargeRate(10.04m, 5m);
+
+            Assert.AreEqual(10.04m, chargeRate.TaxRate);
+            Assert.AreEqual(5m, chargeRate.ImportDutyRate);
+        }
+
+        /// <summary>
+        /// Set tax rate to zero.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChargeRate_SetTaxRateToZero_Fail()
+        {
+            var chargeRate = new ChargeRate(0, 5m);
+        }
+
+        /// <summary>
+        /// Set tax rate to negative value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChargeRate_SetNegativeTaxRate_Fail()
+        {
+            var chargeRate = new ChargeRate(-3.2m, 5m);
+        }
+
+        /// <summary>
+        /// Set import duty rate to zero.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChargeRate_SetImportDutyRateToZero_Fail()
+        {
+            var chargeRate = new ChargeRate(12m, 0);
+        }
+
+        /// <summary>
+        /// Set import duty rate to negative value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChargeRate_SetNegativeImportDutyRate_Fail()
+        {
+            var chargeRate = new ChargeRate(14.2m, -34.7m);
+        }
+    }
+}
---
+++ UnitTests/ProductTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ST.BusinessLogic.Tests
+{
+    /// <summary>
+    /// Test product class.
+    /// </summary>
+    [TestClass]
+    public class ProductTests
+    {
+        /// <summary>
+        /// Create product using default constructor.
+        /// </summary>
+        [TestMethod]
+        public void Product_CreateWithoutParameters_Success()
+        {
+            var product = new Product();
+
+            Assert.IsNull(product.Title);
+            Assert.AreEqual(0, product.Price);
+            Assert.AreEqual(1, product.Quantity);
+            Assert.AreEqual(ProductType.None, product.ProductType);
+            Assert.IsNull(product.ProductTax);
+        }
+
+        /// <summary>
+        /// Create product using parameterized constructor.
+        /// </summary>
+        [TestMethod]
+        public void Product_CreateWithParameters_Success()
+        {
+            var product = new Product("book", 18.99m, 1, ProductType.Book);
+
+            Assert.AreEqual("book", product.Title);
+            Assert.AreEqual(18.99m, product.Price);
+            Assert.AreEqual(1, product.Quantity);
+            Assert.AreEqual(ProductType.Book, product.ProductType);
+            Assert.IsNull(product.ProductTax);
+        }
+
+        /// <summary>
+        /// Set product title to an empty string.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Product_SetEmptyTitle_Fail()
+        {
+            var product = new Product();
+            product.Title = "";
+        }
+
+        /// <summary>
+        /// Set product title to null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Product_SetTitleToNull_Fail()
+        {
+            var product = new Product();
+
+            product.Title = null;
+        }
+
+        /// <summary>
+        /// Set product price to negative value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Product_SetNegativePrice_Fail()
+        {
+            var product = new Product { Price = -23.45m };
+        }
+
+        /// <summary>
+        /// Set product price to zero.
+        /// </summary>
+        [TestMethod]
+        public void Product_SetPriceToZero_Fail()
+        {
+            var product = new Product { Price = 0 };
+            Assert.AreEqual(0, product.Price);
+        }
+
+        /// <summary>
+        /// Set product quantity to negative value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Product_SetNegativeQuantity_Fail()
+        {
+            var product = new Product { Quantity = -4 };
+        }
+
+        /// <summary>
+        /// Set product quantity to zero.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Product_SetQuantityToZero_Success()
+        {
+            var product = new Product { Quantity = 0 };
+        }
+
+        /// <summary>
+        /// Set product tax to negative value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Product_SetNegativeTax_Fail()
+        {
+            var product = new Product { ProductTax = -23.45m };
+        }
+
+        /// <summary>
+        /// Set product tax to zero.
+        /// </summary>
+        [TestMethod]
+        public void Product_SetTaxToZero_Fail()
+        {
+            var product = new Product { ProductTax = 0 };
+            Assert.AreEqual(0, product.ProductTax);
+        }
+    }
+}
