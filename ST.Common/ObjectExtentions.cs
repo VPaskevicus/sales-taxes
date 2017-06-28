041b025bfb6a752e9d5a72b4a02c53de0657aeaa
Vladimirs Paskevicus <v.paskevicus@outlook.com>
2017-06-28 20:49:18 +0100

    #11 - Adding string and object extension methods.

---
+++ ST.Common/ObjectExtentions.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace ST.Common
+{
+    public static class ObjectExtentions
+    {
+        public static string ToJsonString(this object o)
+        {
+            return JsonConvert.SerializeObject(o);
+        }
+    }
+}
